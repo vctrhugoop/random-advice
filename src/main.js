a287d1e0d3433213f7141180240a88313e71e907
vctrhugoop <vhdestroy@hotmail.com>
2023-11-13 20:29:34 -0300

    commit project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const adviceId = document.querySelector('.advice-id');
+const adviceContent = document.querySelector('.advice-content');
+const diceButton = document.querySelector('.dice-button');
+
+function getAdvice(data) {
+  fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+      const adviceJson = data['slip'];
+
+      adviceId.innerHTML = `ADVICE #${adviceJson['id']}`;
+      adviceContent.innerHTML = `"${adviceJson['advice']}"`;
+    });
+}
+
+getAdvice();
+
+diceButton.addEventListener('click', () => {
+  getAdvice();
+});
